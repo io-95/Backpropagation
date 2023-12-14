565e9a1680309765d9802e0f7b7c78515b68f8e7
io-95 <77495768+io-95@users.noreply.github.com>
2023-12-14 23:37:27 +0100

    add read_data function to main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 def main():
-    pass
+    data = []
+    read_data()
 
-if __name__ == __main__:
+def read_data(data):
+    import csv
+    with open('app1.data', 'r') as fd:
+        reader = csv.reader(fd)
+        for row in reader:
+            data.append(list(map(float, row)))
+    length_d = len(data)
+
+if __name__ == '__main__':
     main()
