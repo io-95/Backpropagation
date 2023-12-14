ec41f3e5ad8d4f344e46febae01c66fe9b677530
io-95 <77495768+io-95@users.noreply.github.com>
2023-12-14 23:41:59 +0100

    change file name in read_data function

--- main.py
+++ main.py
@@ -2,15 +2,15 @@
 
 def main():
     data = []
-    read_data()
+    read_data(data)
 
 def read_data(data):
     import csv
-    with open('app1.data', 'r') as fd:
+    with open('data.txt', 'r') as fd:
         reader = csv.reader(fd)
         for row in reader:
-            data.append(list(map(float, row)))
-    length_d = len(data)
+            data.append(list(row))
+    #length_d = len(data)
 
 if __name__ == '__main__':
     main()
