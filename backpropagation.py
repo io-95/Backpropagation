4ee6b3c3a84ec730a2e685e5f09739f9e8917769
io-95 <77495768+io-95@users.noreply.github.com>
2023-12-18 23:20:43 +0100

    calculation of hidden layer

--- backpropagation.py
+++ backpropagation.py
@@ -4,6 +4,12 @@ def backpropagation(data, learningrate):
     weight_layer_1 = []
     weight_layer_2 = []
     weight_layer_1, weight_layer_2 = initialize_weight(weight_layer_1, weight_layer_2)
+    
+    neuron = [0, 0]
+    y = 0
+    for i in range(len(data)):
+        neuron[0] = data[i][0] * weight_layer_1[0][0] + data[i][1] * weight_layer_1[1][0]
+        neuron[1] = data[i][0] * weight_layer_1[0][1] + data[i][1] * weight_layer_1[1][1] 
 
 def initialize_weight(weight_layyer1, weight_layer_2):
     #initilize a list with 4 rows and 2 columns
