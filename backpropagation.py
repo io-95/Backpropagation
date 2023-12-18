bfba0f8edc1c7d122a43f21776910a636c446796
io-95 <77495768+io-95@users.noreply.github.com>
2023-12-18 23:34:54 +0100

    calculate quadratical error

--- backpropagation.py
+++ backpropagation.py
@@ -8,12 +8,19 @@ def backpropagation(data, learningrate):
     
     neuron = [0, 0]
     y = 0
+    quad_error = 0
     for i in range(len(data)):
+        # forwardpropagation
         neuron[0] = data[i][0] * weight_layer_1[0][0] + data[i][1] * weight_layer_1[1][0]
         neuron[1] = data[i][0] * weight_layer_1[0][1] + data[i][1] * weight_layer_1[1][1] 
         neuron[0] = sigmoid(neuron[0])
         neuron[1] = sigmoid(neuron[1])
-        
+        y = neuron[0] * weight_layer_2[0] + neuron[1] + weight_layer_2[1]
+        quad_error = 0.5 * (data[i][2] - y) ** 2
+    
+        # backpropagation
+
+
 def initialize_weight(weight_layyer1, weight_layer_2):
     #initilize a list with 4 rows and 2 columns
     weight_layer_1 = [[0]*2]*2
