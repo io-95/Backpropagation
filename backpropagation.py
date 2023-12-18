cae6b0511d8bcc9590a94bad97374ea3368d05ae
io-95 <77495768+io-95@users.noreply.github.com>
2023-12-18 23:24:14 +0100

    add activation function

--- backpropagation.py
+++ backpropagation.py
@@ -1,4 +1,5 @@
 from random import random
+import numpy as np
 
 def backpropagation(data, learningrate):
     weight_layer_1 = []
@@ -10,7 +11,9 @@ def backpropagation(data, learningrate):
     for i in range(len(data)):
         neuron[0] = data[i][0] * weight_layer_1[0][0] + data[i][1] * weight_layer_1[1][0]
         neuron[1] = data[i][0] * weight_layer_1[0][1] + data[i][1] * weight_layer_1[1][1] 
-
+        neuron[0] = sigmoid(neuron[0])
+        neuron[1] = sigmoid(neuron[1])
+        
 def initialize_weight(weight_layyer1, weight_layer_2):
     #initilize a list with 4 rows and 2 columns
     weight_layer_1 = [[0]*2]*2
@@ -24,3 +27,6 @@ def initialize_weight(weight_layyer1, weight_layer_2):
         weight_layer_2.append(random())
 
     return weight_layer_1, weight_layer_2
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
